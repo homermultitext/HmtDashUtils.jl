d78c5ea9c4aaa1fea7bc5ad8757ab929c9901fd1
Neel Smith <dnsmith.neel@gmail.com>
2022-03-28 09:28:34 -0400

    Add `md_dseoverview` method

--- dashboards/test_simpletext.jl
+++ dashboards/test_simpletext.jl
@@ -45,11 +45,7 @@ end
 app = dash(assets_folder = assetfolder, include_assets_files=true)
 
 app.layout = html_div(className = "w3-container") do
-    html_div(className = "w3-container w3-light-gray w3-cell w3-mobile w3-border-left  w3-border-right w3-border-gray", 
-        children = [
-        dcc_markdown("*Dashboard version*: **$(DASHBOARD_VERSION)** ([version notes](https://homermultitext.github.io/dashboards/alpha-search/))")
-        ]
-    ),
+
     
     html_h1(
         dcc_markdown("`HmtDashUtils`: simple text display")),
